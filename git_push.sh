9dc3b244e56ab6d3d851ba2c3ddc7f68a6ce4cdb
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-06-17 09:55:04 +0530

    Main Push

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 echo "# Accident-Analysis" >> README.md
 git init
-git add README.md git_push.sh main.py
+git add README.md git_push.sh main.py driver.csv spatiotemporal.csv
 git commit -m "Main Push"
 git remote add origin https://github.com/Goldenprince8420/Accident-Analysis.git
 git remote -v
