a3bf30860039f28fa7f212b48d959e93dc735de8
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-06-17 09:51:00 +0530

    Main Push

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 echo "# Accident-Analysis" >> README.md
 git init
 git add README.md git_push.sh main.py
-git commit -m "first commit"
-git branch -M main
+git commit -m "Main Push"
 git remote add origin https://github.com/Goldenprince8420/Accident-Analysis.git
-git push -u origin main
+git remote -v
+git push origin main
